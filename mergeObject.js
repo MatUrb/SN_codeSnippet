58dcde1d7043ee3336f59ff81cf5a57a985990c6
MatUrb <40583074+MatUrb@users.noreply.github.com>
2023-01-25 17:34:26 +1000

    Update mergeObject.js

--- mergeObject.js
+++ mergeObject.js
@@ -1,19 +1,20 @@
 /* This merge Objects since Object.assign is not available server-side (script include, BR,...) 
-@param Takes any number of objectt passed in as parameter 
+@param Takes any number of object passed in as parameter
+@output object : merged object from what was passed as arguments;
 */
 
 _mergeObj: function(){
-		var resObj = {};
-		for (var i = 0; i < arguments.length; i++) {
-    var el = arguments[i];
-    var isObject = (typeof el === 'object' && el !== null && !Array.isArray(el));
-			if(!isObject)
-				continue;
-			for (var prop in arguments[i]) {
-				if (arguments[i].hasOwnProperty(prop))
-					resObj[prop] = arguments[i][prop];
-			} 
-		}
-		return resObj;
+	var resObj = {};
+	for (var i = 0; i < arguments.length; i++) {
+var el = arguments[i];
+var isObject = (typeof el === 'object' && el !== null && !Array.isArray(el));
+		if(!isObject)
+			continue;
+		for (var prop in arguments[i]) {
+			if (arguments[i].hasOwnProperty(prop))
+				resObj[prop] = arguments[i][prop];
+		} 
 	}
-  
+	return resObj;
+}
+
