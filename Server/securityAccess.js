1dd700647514f0d8e0ed20efa763f3052201fbe8
MatUrb <40583074+MatUrb@users.noreply.github.com>
2023-02-02 22:09:42 +1000

    Update securityAccess.js

--- Server/securityAccess.js
+++ Server/securityAccess.js
@@ -7,8 +7,8 @@ answer = new securityAccess().canEdit(current);
 or in Before Query BR:
 
 var encodedQuery = new securityAccess().getBRQuery(current);
-	if(encodedQuery)
-		current.addEncodedQuery(encodedQuery);
+if(encodedQuery)
+	current.addEncodedQuery(encodedQuery);
 
 */
 
@@ -18,70 +18,70 @@ securityAccess.prototype = {
 
 initialize: function() {
 this.TABLES = "map of table JSON object" ;
-  this.ROLES = "map of roles JSON object" ;
+this.ROLES = "map of roles JSON object" ;
 
 this.ACL_CONDITION = {};
 this.ACL_CONDITION[this.TABLES] = {canRead: {}, canEdit: {}}; // define the handler require for the table
 this.ACL_CONDITION[this.TABLES].canEdit[this.ROLES] = ["array of encodedQuery that will be join with an OR operator"].join('^NQ'); // for each handler define the roles targeted;
-
 /* repeat as necessary */
-
+},
+	
 /*  This is used for query business rules this could replace a canRead ACL */
 getBRQuery: function(current){
-		var user = userId ? gs.getUserByID(userId) : gs.getUser(),
-			table = record.getTableName(),
-			encodedQuery = '';
+	var user = userId ? gs.getUserByID(userId) : gs.getUser(),
+		table = record.getTableName(),
+		encodedQuery = '';
 
-		if(!this.ACL_CONDITION[table])
-			return encodedQuery;
+	if(!this.ACL_CONDITION[table])
+		return encodedQuery;
 
 /* Role based condition to handle the different scenarios */
-		switch(true){
-			case user.hasRole(this.ROLES):
-				break;
-			case user.hasRole(this.ROLES):
-				if(this.ACL_CONDITION[table].canRead[this.ROLES])
-					encodedQuery = this.ACL_CONDITION[table].canRead[this.ROLES];
-				break;
-			case user.hasRole(this.ROLES):
-				if( this.ACL_CONDITION[table].canRead[this.ROLES])
-					encodedQuery = this.ACL_CONDITION[table].canRead[this.ROLES];
-				break;
-			default:
-				break;
-		}
+	switch(true){
+		case user.hasRole(this.ROLES):
+			break;
+		case user.hasRole(this.ROLES):
+			if(this.ACL_CONDITION[table].canRead[this.ROLES])
+				encodedQuery = this.ACL_CONDITION[table].canRead[this.ROLES];
+			break;
+		case user.hasRole(this.ROLES):
+			if( this.ACL_CONDITION[table].canRead[this.ROLES])
+				encodedQuery = this.ACL_CONDITION[table].canRead[this.ROLES];
+			break;
+		default:
+			break;
+	}
 
-		return encodedQuery;		
-	},
+	return encodedQuery;		
+},
 
 
 /* CRUD Handler */
 canEdit: function(record,userId){
-		var user = userId ? gs.getUserByID(userId) : gs.getUser(),
-			table = record.getTableName(),
-			canEdit = false;
+	var user = userId ? gs.getUserByID(userId) : gs.getUser(),
+		table = record.getTableName(),
+		canEdit = false;
 
-		if(!this.ACL_CONDITION[table])
-			return canEdit;
+	if(!this.ACL_CONDITION[table])
+		return canEdit;
 
-		switch(true){
-			case user.hasRole(this.ROLES.ADMIN):
-				canEdit = true;
-				break;
-			case user.hasRole(this.ROLES.ROLE_1):
-				if(this.ACL_CONDITION[table].canEdit[this.ROLES.ROLE_1])
-					canEdit = GlideFilter.checkRecord(record, this.ACL_CONDITION[table].canEdit[this.ROLES.ROLE_1]);
-				break;
-			case user.hasRole(this.ROLES.ROLE_2):
-				if(this.ACL_CONDITION[table].canEdit[this.ROLES.SOLUTION_CONSULTANT])
-					canEdit = GlideFilter.checkRecord(record, this.ACL_CONDITION[table].canEdit[this.ROLES.ROLE_2]);
-				break;
-			default:
-				break;
-		}
-
-		return canEdit;	
+	switch(true){
+		case user.hasRole(this.ROLES.ADMIN):
+			canEdit = true;
+			break;
+		case user.hasRole(this.ROLES.ROLE_1):
+			if(this.ACL_CONDITION[table].canEdit[this.ROLES.ROLE_1])
+				canEdit = GlideFilter.checkRecord(record, this.ACL_CONDITION[table].canEdit[this.ROLES.ROLE_1]);
+			break;
+		case user.hasRole(this.ROLES.ROLE_2):
+			if(this.ACL_CONDITION[table].canEdit[this.ROLES.SOLUTION_CONSULTANT])
+				canEdit = GlideFilter.checkRecord(record, this.ACL_CONDITION[table].canEdit[this.ROLES.ROLE_2]);
+			break;
+		default:
+			break;
 	}
-  
-  type: "securityAccess"
+
+	return canEdit;	
+},
+
+type: "securityAccess"
 }
